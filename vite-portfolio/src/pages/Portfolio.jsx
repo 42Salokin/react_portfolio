382acaa7d2a7b14fa9566f318226ddca9e78b596
Salokin <nicholas.dpoul@gmail.com>
2024-06-04 19:03:01 -0400

    created projects file to hold objects array of past projects, added screenshots to assets folder

--- vite-portfolio/src/pages/Portfolio.jsx
+++ vite-portfolio/src/pages/Portfolio.jsx
@@ -10,3 +10,5 @@ export default function About() {
     </Container>
   );
 }
+// TODO: create object array for projects, with properties for image, GitHub repo, and deployed site
+// iterate over array, creating display card for each project
---
+++ vite-portfolio/src/utils/projects.jsx
@@ -0,0 +1,45 @@
+import gottaCode from "./assets/gotta_code.png";
+import wildWellness from "./assets/wild_wellness.png";
+import techBlog from "./assets/tech_blog.png";
+import weather from "./assets/weather.png";
+import readme from "./assets/readme.png";
+import employee from "./assets/employee.png";
+
+export default projects = [
+    {
+        name: "Gotta Code 'em All!",
+        image: gottaCode,
+        repo: "https://github.com/42Salokin/gotta_code",
+        site: "https://gotta-code.onrender.com/"
+    },
+    {
+        name: "Wild Wild Wellness",
+        image: wildWellness,
+        repo: "https://github.com/mileswildmore18/The-Wild-Wild-Wellness",
+        site: "https://mileswildmore18.github.io/The-Wild-Wild-Wellness/"
+    },
+    {
+        name: "Tech Blog",
+        image: techBlog,
+        repo: "https://github.com/42Salokin/tech_blog",
+        site: "https://tech-blog-p0i5.onrender.com"
+    },
+    {
+        name: "Weather Dashboard",
+        image: weather,
+        repo: "https://github.com/42Salokin/weather_dashboard",
+        site: "https://42salokin.github.io/weather_dashboard/"
+    },
+    {
+        name: "README generator",
+        image: readme,
+        repo: "https://github.com/42Salokin/readme_generator",
+        site: "https://drive.google.com/file/d/1wiCMhYmYhiqAFMPGFIrUTbyBopdYjygs/view"
+    },
+    {
+        name: "Employee Tracker",
+        image: employee,
+        repo: "https://github.com/42Salokin/employee_tracker",
+        site: "https://drive.google.com/file/d/19pvCs-z8CcpzPtOtMlYaW3lLp8jQ-J1G/view"
+    },
+]
